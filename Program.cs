25eac11202c23d94d9e34fece9ab5743fbf27b49
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-05-05 01:16:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter any string to Reverse: ");
+            string name = Console.ReadLine();
+
+            string revname = "";
+
+            foreach(char item in name)
+            {
+                revname = item + revname;
+            }
+            Console.WriteLine(revname + " Revers of " + name);
+
+            Console.ReadLine();
+        }
+    }
+}
